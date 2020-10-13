a34114bad3c8e9e77ad29ba8812c151afdac358a
ugo <ugovaretto@gmail.com>
2020-10-13 15:15:26 +0800

    Added aws profile, renamed

--- src/parallel_upload.cpp
+++ src/parallel_upload.cpp
@@ -52,7 +52,7 @@ using namespace std;
 using namespace filesystem;
 
 //------------------------------------------------------------------------------
-struct Args {
+struct Config {
     bool showHelp = false;
     string s3AccessKey;
     string s3SecretKey;
@@ -61,17 +61,18 @@ struct Args {
     string key;
     string file;
     string credentials;
+    string awsProfile;
     int jobs = 1;
 };
 
-void Validate(const Args& args) {
-    if (args.s3AccessKey.empty() && !args.s3SecretKey.empty() ||
-        args.s3SecretKey.empty() && !args.s3AccessKey.empty()) {
+void Validate(const Config& config) {
+    if (config.s3AccessKey.empty() && !config.s3SecretKey.empty() ||
+        config.s3SecretKey.empty() && !config.s3AccessKey.empty()) {
         throw invalid_argument(
             "ERROR: both access and secret keys have to be specified");
     }
 #ifdef VALIDATE_URL
-    const URL url = ParseURL(args.endpoint);
+    const URL url = ParseURL(config.endpoint);
     if (url.proto != "http" && url.proto != "https") {
         throw invalid_argument(
             "ERROR: only 'http' and 'https' protocols supported");
@@ -92,15 +93,15 @@ void Validate(const Args& args) {
 using Headers = map<string, string>;
 using Parameters = map<string, string>;
 
-WebRequest BuildUploadRequest(const Args& args, const string& path, int partNum,
-                              const string& uploadId) {
+WebRequest BuildUploadRequest(const Config& config, const string& path,
+                              int partNum, const string& uploadId) {
     Parameters params = {{"partNumber", to_string(partNum + 1)},
                          {"uploadId", uploadId}};
     auto signedHeaders =
-        SignHeaders(args.s3AccessKey, args.s3SecretKey, args.endpoint, "PUT",
-                    args.bucket, args.key, "", params);
+        SignHeaders(config.s3AccessKey, config.s3SecretKey, config.endpoint,
+                    "PUT", config.bucket, config.key, "", params);
     Headers headers(begin(signedHeaders), end(signedHeaders));
-    WebRequest req(args.endpoint, path, "PUT", params, headers);
+    WebRequest req(config.endpoint, path, "PUT", params, headers);
     return req;
 }
 
@@ -123,24 +124,25 @@ string BuildEndUploadXML(vector<future<string>>& etags) {
     return xml;
 }
 
-WebRequest BuildEndUploadRequest(const Args& args, const string& path,
+WebRequest BuildEndUploadRequest(const Config& config, const string& path,
                                  vector<future<string>>& etags,
                                  const string& uploadId) {
     Parameters params = {{"uploadId", uploadId}};
     auto signedHeaders =
-        SignHeaders(args.s3AccessKey, args.s3SecretKey, args.endpoint, "POST",
-                    args.bucket, args.key, "", params);
+        SignHeaders(config.s3AccessKey, config.s3SecretKey, config.endpoint,
+                    "POST", config.bucket, config.key, "", params);
     Headers headers(begin(signedHeaders), end(signedHeaders));
-    WebRequest req(args.endpoint, path, "POST", params, headers);
+    WebRequest req(config.endpoint, path, "POST", params, headers);
     req.SetMethod("POST");
     req.SetPostData(BuildEndUploadXML(etags));
     return req;
 }
 
-string UploadPart(const Args& args, const string& path, const string& uploadId,
-                  int i, size_t offset, size_t chunkSize) {
-    WebRequest ul = BuildUploadRequest(args, path, i, uploadId);
-    const bool ok = ul.UploadFile(args.file, offset, chunkSize);
+string UploadPart(const Config& config, const string& path,
+                  const string& uploadId, int i, size_t offset,
+                  size_t chunkSize) {
+    WebRequest ul = BuildUploadRequest(config, path, i, uploadId);
+    const bool ok = ul.UploadFile(config.file, offset, chunkSize);
     if (!ok) {
         throw(runtime_error("Cannot upload chunk " + to_string(i + 1)));
     }
@@ -153,45 +155,52 @@ string UploadPart(const Args& args, const string& path, const string& uploadId,
     return etag;
 }
 
-void InitArgs(Args& args) {
-    if (!args.s3AccessKey.empty() && !args.s3SecretKey.empty()) return;
-    const string fname = args.credentials.empty()
+void InitConfig(Config& config) {
+    if (!config.s3AccessKey.empty() && !config.s3SecretKey.empty()) return;
+    const string fname = config.credentials.empty()
                              ? GetHomeDir() + "/.aws/credentials"
-                             : args.credentials;
+                             : config.credentials;
+    config.awsProfile =
+        config.awsProfile.empty() ? "default" : config.awsProfile;
     Toml toml = ParseTomlFile(fname);  // only default profile supported
-    args.s3AccessKey = toml["default"]["aws_access_key_id"];
-    args.s3SecretKey = toml["default"]["aws_secret_access_key"];
+    config.s3AccessKey = toml[config.awsProfile]["aws_access_key_id"];
+    config.s3SecretKey = toml[config.awsProfile]["aws_secret_access_key"];
 }
 
 //------------------------------------------------------------------------------
 int main(int argc, char const* argv[]) {
     try {
-        Args args;
+        Config config;
         auto cli =
-            lyra::help(args.showHelp).description("Upload file to S3 bucket") |
-            lyra::opt(args.s3AccessKey,
+            lyra::help(config.showHelp)
+                .description("Upload file to S3 bucket") |
+            lyra::opt(config.s3AccessKey,
                       "awsAccessKey")["-a"]["--access_key"]("AWS access key")
                 .optional() |
-            lyra::opt(args.s3SecretKey,
+            lyra::opt(config.s3SecretKey,
                       "awsSecretKey")["-s"]["--secret_key"]("AWS secret key")
                 .optional() |
-            lyra::opt(args.endpoint,
+            lyra::opt(config.endpoint,
                       "endpoint")["-e"]["--endpoint"]("Endpoing URL")
                 .required() |
-            lyra::opt(args.bucket, "bucket")["-b"]["--bucket"]("Bucket name")
+            lyra::opt(config.bucket, "bucket")["-b"]["--bucket"]("Bucket name")
                 .required() |
-            lyra::opt(args.key, "key")["-k"]["--key"]("Key name").required() |
-            lyra::opt(args.file, "file")["-f"]["--file"]("File name")
+            lyra::opt(config.key, "key")["-k"]["--key"]("Key name").required() |
+            lyra::opt(config.file, "file")["-f"]["--file"]("File name")
                 .required() |
-            lyra::opt(args.jobs, "parallel jobs")["-j"]["--jobs"](
+            lyra::opt(config.jobs, "parallel jobs")["-j"]["--jobs"](
                 "Number inputFile parallel jobs")
                 .optional() |
-            lyra::opt(args.credentials,
+            lyra::opt(config.credentials,
                       "credentials file")["-c"]["--credentials"](
                 "Credentials file, AWS cli format")
+                .optional() |
+            lyra::opt(config.awsProfile,
+                      "AWS config profile")["-p"]["--profile"](
+                "Profile in AWS config file")
                 .optional();
 
-        InitArgs(args);
+        InitConfig(config);
 
         // Parse the program arguments:
         auto result = cli.parse({argc, argv});
@@ -200,98 +209,102 @@ int main(int argc, char const* argv[]) {
             cerr << cli << endl;
             exit(1);
         }
-        if (args.showHelp) {
+        if (config.showHelp) {
             cout << cli;
             return 0;
         }
-        Validate(args);
-        string path = "/" + args.bucket + "/" + args.key;
+        Validate(config);
+        FILE* inputFile = fopen(config.file.c_str(), "rb");
+        if (!inputFile) {
+            throw runtime_error(string("cannot open file ") + config.file);
+        }
+        fclose(inputFile);
+        string path = "/" + config.bucket + "/" + config.key;
 
-        if (args.jobs > 1) {
+        if (config.jobs > 1) {
             // retrieve file size
-            const size_t fileSize = FileSize(args.file);
+            const size_t fileSize = FileSize(config.file);
             // compute chunk size
-            const size_t chunkSize = fileSize / args.jobs;
+            const size_t chunkSize = fileSize / config.jobs;
             // compute last chunk size
-            const size_t lastChunkSize =
-                fileSize % args.jobs == 0 ? chunkSize : fileSize % args.jobs;
+            const size_t lastChunkSize = fileSize % config.jobs == 0
+                                             ? chunkSize
+                                             : fileSize % config.jobs;
             // initiate request
-            auto signedHeaders = SignHeaders(args.s3AccessKey, args.s3SecretKey,
-                                             args.endpoint, "POST", args.bucket,
-                                             args.key, "", {{"uploads=", ""}});
+            auto signedHeaders = SignHeaders(
+                config.s3AccessKey, config.s3SecretKey, config.endpoint, "POST",
+                config.bucket, config.key, "", {{"uploads=", ""}});
             map<string, string> headers(begin(signedHeaders),
                                         end(signedHeaders));
-            WebRequest req(args.endpoint, path, "POST", {{"uploads=", ""}},
+            WebRequest req(config.endpoint, path, "POST", {{"uploads=", ""}},
                            headers);
-            req.Send();
             if (!req.Send()) {
                 throw runtime_error("Error sending request: " + req.ErrorMsg());
             }
             if (req.StatusCode() >= 400) {
-                vector<uint8_t> resp2 = req.GetContent();
-                const string xml2(begin(resp2), end(resp2));
-                const string errcode = XMLTag(xml2, "[Cc]ode");
+                vector<uint8_t> resp = req.GetContent();
+                const string xml(begin(resp), end(resp));
+                const string errcode = XMLTag(xml, "[Cc]ode");
                 throw runtime_error("Error sending begin upload request - " +
                                     errcode);
             }
             vector<uint8_t> resp = req.GetContent();
             const string xml(begin(resp), end(resp));
             const string uploadId = XMLTag(xml, "[Uu]pload[Ii][dD]");
-            vector<future<string>> etags(args.jobs);
-            FILE* inputFile = fopen(args.file.c_str(), "rb");
-            if (!inputFile) {
-                throw runtime_error(string("cannot open file ") + args.file);
-            }
-            fclose(inputFile);
-            if (fileSize % args.jobs == 0) {
-                for (int i = 0; i != args.jobs; ++i) {
-                    etags[i] = async(launch::async, UploadPart, args, path,
+            vector<future<string>> etags(config.jobs);
+
+            if (fileSize % config.jobs == 0) {
+                for (int i = 0; i != config.jobs; ++i) {
+                    etags[i] = async(launch::async, UploadPart, config, path,
                                      uploadId, i, chunkSize * i, chunkSize);
                 }
             } else {
-                for (int i = 0; i != args.jobs - 1; ++i) {
-                    etags[i] = async(launch::async, UploadPart, args, path,
+                for (int i = 0; i != config.jobs - 1; ++i) {
+                    etags[i] = async(launch::async, UploadPart, config, path,
                                      uploadId, i, chunkSize * i, chunkSize);
                 }
-                for (int j = 0; j != args.jobs - 1; ++j) {
+                for (int j = 0; j != config.jobs - 1; ++j) {
                     etags[j].wait();
                 }
-                const size_t offset = chunkSize * (args.jobs - 1);
-                const int idx = args.jobs - 1;
-                etags[args.jobs - 1] =
-                    async(launch::async, UploadPart, args, path, uploadId, idx,
-                          offset, lastChunkSize);
+                const size_t offset = chunkSize * (config.jobs - 1);
+                const int idx = config.jobs - 1;
+                etags[config.jobs - 1] =
+                    async(launch::async, UploadPart, config, path, uploadId,
+                          idx, offset, lastChunkSize);
             }
             WebRequest endUpload =
-                BuildEndUploadRequest(args, path, etags, uploadId);
+                BuildEndUploadRequest(config, path, etags, uploadId);
             if (!endUpload.Send()) {
                 throw runtime_error("Error sending request: " + req.ErrorMsg());
             }
             if (endUpload.StatusCode() >= 400) {
-                vector<uint8_t> resp2 = endUpload.GetContent();
-                const string xml2(begin(resp2), end(resp2));
-                const string errcode = XMLTag(xml2, "[Cc]ode");
+                vector<uint8_t> resp = endUpload.GetContent();
+                const string xml(begin(resp), end(resp));
+                const string errcode = XMLTag(xml, "[Cc]ode");
                 throw runtime_error("Error sending end unpload request - " +
                                     errcode);
             }
             vector<uint8_t> resp2 = endUpload.GetContent();
             const string xml2(begin(resp2), end(resp2));
             const string etag = XMLTag(xml2, "[Ee][Tt]ag");
+            if(etag.empty()) {
+                cerr << "Error sending end upload request" << endl;
+            }
             cout << etag << endl;
         } else {
-            auto signedHeaders =
-                SignHeaders(args.s3AccessKey, args.s3SecretKey, args.endpoint,
-                            "PUT", args.bucket, args.key, "");
+            auto signedHeaders = SignHeaders(
+                config.s3AccessKey, config.s3SecretKey, config.endpoint, "PUT",
+                config.bucket, config.key, "");
             map<string, string> headers(begin(signedHeaders),
                                         end(signedHeaders));
-            WebRequest req(args.endpoint, path, "PUT", {}, headers);
-            if (!req.UploadFile(args.file)) {
+            WebRequest req(config.endpoint, path, "PUT", {}, headers);
+            if (!req.UploadFile(config.file)) {
                 throw runtime_error("Error sending request: " + req.ErrorMsg());
             }
             if (req.StatusCode() >= 400) {
-                vector<uint8_t> resp2 = req.GetContent();
-                const string xml2(begin(resp2), end(resp2));
-                const string errcode = XMLTag(xml2, "[Cc]ode");
+                vector<uint8_t> resp = req.GetContent();
+                const string xml(begin(resp), end(resp));
+                const string errcode = XMLTag(xml, "[Cc]ode");
                 throw runtime_error("Error sending end unpload request - " +
                                     errcode);
             }
@@ -299,6 +312,9 @@ int main(int argc, char const* argv[]) {
             const string hhs(begin(hh), end(hh));
             const string etag = HTTPHeader(hhs, "[Ee][Tt]ag");
             cout << etag << endl;
+            if(etag.empty()) {
+                cerr << "Error sending upload request" << endl;
+            }
         }
         return 0;
     } catch (const exception& e) {
